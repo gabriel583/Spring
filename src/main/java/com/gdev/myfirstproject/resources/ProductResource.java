3a731fde1befa5e074eceb3ca4030d29734fe3f1
Gabriel <gabrielsilva583@hotmail.com>
2021-11-18 21:32:03 +0000

    Add Products in memory

---
+++ src/main/java/com/gdev/myfirstproject/resources/ProductResource.java
@@ -0,0 +1,35 @@
+package com.gdev.myfirstproject.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gdev.myfirstproject.entities.Product;
+import com.gdev.myfirstproject.repositories.ProductRepository;
+
+@RestController
+@RequestMapping(value = "/products")
+
+public class ProductResource {
+
+	@Autowired
+	private ProductRepository ProductRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll() {
+		List<Product> list = ProductRepository.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id) {
+		Product cat = ProductRepository.findById(id);
+		return ResponseEntity.ok().body(cat);
+	}
+	
+}
